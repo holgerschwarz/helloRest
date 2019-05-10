3790ad16b6cbe7dbbce073be108f84492b14478c
schwarz <holgi.schwarz@web.de>
2019-05-10 13:33:05 +0200

    c

---
+++ src/main/resources/prepareDocker.sh
@@ -0,0 +1,24 @@
+#install docker for ubuntu 18.10
+#Uninstall old versions
+sudo apt-get remove docker docker-engine docker-ce docker.io -y
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get install docker-ce -y
+
+sudo docker container run hello-world
+
+docker -v
